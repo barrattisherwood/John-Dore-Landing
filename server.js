a1ca2cdc4babcdc8c399468084b1cabca60bb5a4
Barratt Isherwood <b4rr4tt@gmail.com>
2019-03-18 23:05:00 +0200

    fix(hosting): potential heroku fix

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ app.get('/*', function (req, res) {
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080, (res) => {
+app.listen(process.env.PORT || 3000, (res) => {
 
 });
