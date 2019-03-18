37a30e9342857b9aa28b1a9103ad698adafca5ad
Barratt Isherwood <b4rr4tt@gmail.com>
2019-03-18 22:11:04 +0200

    fix(deploy): upgraded to Angular 7 and added heroku build configs

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/john-dore-flooring-landing-app'));
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
+
+app.get('/*all', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+  });
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/john-dore-flooring-landing-app/index.html'));
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
