61736edd927b9dd43ed25c5b3046c6f258f8a30e
Barratt Isherwood <b4rr4tt@gmail.com>
2019-03-23 16:05:03 +0200

    chore(deploy): deploying all added files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { GridComponent } from './components/grid/grid.component';
+import { MapComponent } from './components/map/map.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HeroComponent,
+    GridComponent,
+    MapComponent,
+    ContactFormComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
